624c109acd8a0c6cffe24b83bee3b688f47b52ce
Newwer21 <newwer21@MacBook-Pro-de-Ly.local>
2015-04-07 22:01:40 +0200

    debug...

--- index.php
+++ index.php
@@ -50,29 +50,29 @@
 	echo '<a href="'.$loginUrl.'">Se connecter</a>';
 	// phpinfo();
 
-	if (isset($_SESSION) && isset($_SESSION['fb_token']))
-	{
-		$session = new FacebookSession($_SESSION['fb_token']);
-	}
-	else
-	{
-		$session = $helper->getSessionFromRedirect();
-	}
+	// if (isset($_SESSION) && isset($_SESSION['fb_token']))
+	// {
+	// 	$session = new FacebookSession($_SESSION['fb_token']);
+	// }
+	// else
+	// {
+	// 	$session = $helper->getSessionFromRedirect();
+	// }
 
-	if ($session) {
-		try {
+	// if ($session) {
+	// 	try {
 			
-			$user_profile = ( new FacebookRequest($session, 'GET', '/me',)->execute()->getGraphObject( GraphUser::className() ) );
+	// 		$user_profile = ( new FacebookRequest($session, 'GET', '/me',)->execute()->getGraphObject( GraphUser::className() ) );
 
-			echo "Nom : " . $user_profile->getName();
+	// 		echo "Nom : " . $user_profile->getName();
 			
-		} catch (Exception $e) {
+	// 	} catch (Exception $e) {
 			
-			echo 'Exception... Code: ' . $e->getCode();
-			echo ' avec message: ' . $e->getMessage();
+	// 		echo 'Exception... Code: ' . $e->getCode();
+	// 		echo ' avec message: ' . $e->getMessage();
 			
-		}
-	}
+	// 	}
+	// }
 
 
 	// var_dump($loginUrl);
