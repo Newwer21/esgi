684f06f0957f1d150021c412098b2cf0b46a3390
Newwer21 <newwer21@MacBook-Pro-de-Ly.local>
2015-04-08 15:31:07 +0200

    skrypcyk

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 	session_start();
 
-	error_reporting( E_ALL );
-
+	error_reporting(E_ALL);
+  ini_set("display_errors", 1);
 
 	use Facebook\FacebookRedirectLoginHelper;
 	use Facebook\FacebookSession;
@@ -13,87 +13,88 @@
 	const APP_ID = 1581981972065213;
 	const APP_SECRET = bb83e05a20109eed97532c6328a24e4e;
 
-	require('facebook/autoload.php');
+    FacebookSession::setDefaultApplication(APPID, APPSECRET);
 
-	FacebookSession::setDefaultApplication(APP_ID, APP_SECRET);
+    $helper = new FacebookRedirectLoginHelper('https://projetesgi1.herokuapp.com/');
 
+    if( isset($_SESSION) &&  isset($_SESSION['fb_token']))
+    {
+        $session  = new FacebookSession($_SESSION['fb_token']);
+    }
+    else
+    {
+        $session = $helper->getSessionFromRedirect();
+    }   
 
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="UTF-8">
+  <head>
+      <meta charset="UTF-8">
+      <title>Titre de ma page</title>   
+      <meta name="description" content="description de ma page">
+     
+  </head>
+  <body>
+       <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '<?php echo APPID;?>',
+            xfbml      : true,
+            version    : 'v2.3'
+          });
+        };
 
-	<title>Faceboook App</title>
-	<script>
-	  window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '<?php echo APP_ID; ?>',
-	      xfbml      : true,
-	      version    : 'v2.3'
-	    });
-	  };
-
-	  (function(d, s, id){
-	     var js, fjs = d.getElementsByTagName(s)[0];
-	     if (d.getElementById(id)) {return;}
-	     js = d.createElement(s); js.id = id;
-	     js.src = "//connect.facebook.net/en_US/sdk.js";
-	     fjs.parentNode.insertBefore(js, fjs);
-	   }(document, 'script', 'facebook-jssdk'));
-	</script>
-</head>
-<body>
-
-<?php 
-	// echo 'oui';
-	$redirectUrl = 'https://esgi.herokuapp.com/';
-
-	$helper = new FacebookRedirectLoginHelper($redirectUrl);
-	$loginUrl = $helper->getLoginUrl(['email, user_birthday']);
-
-	echo '<a href="'.$loginUrl.'">Se connecter</a>';
-	// phpinfo();
-	// echo '$_SESSION '; var_dump($_SESSION);
-
-	if (isset($_SESSION) && isset($_SESSION['fb_token']))
-	{
-		// echo 'session par FacebookSession';
-		$session = new FacebookSession($_SESSION['fb_token']);
-	}
-	else
-	{
-		// echo 'session par $helper';
-		$session = $helper->getSessionFromRedirect();
-	}
-	echo '$_SESSION '; var_dump($_SESSION);
-	echo ' $session '; 
-	var_dump($session);
-
-	if ($session) {
-		// var_dump($session);
-		try {
-			
-			$user_profile = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject( GraphUser::className() );
-
-			echo "Nom : " . $user_profile->getName();
-			
-		} catch (FacebookRequestException $e) {
-			
-			echo 'Exception... Code: ' . $e->getCode();
-			echo ' avec message: ' . $e->getMessage();
-			
-		}
-	}
+        (function(d, s, id){
+           var js, fjs = d.getElementsByTagName(s)[0];
+           if (d.getElementById(id)) {return;}
+           js = d.createElement(s); js.id = id;
+           js.src = "//connect.facebook.net/fr_FR/sdk.js";
+           fjs.parentNode.insertBefore(js, fjs);
+         }(document, 'script', 'facebook-jssdk'));
+      </script>
 
 
-	// var_dump($loginUrl);
+      <h1>Mon application facebook</h1>
+
+      <div
+        class="fb-like"
+        data-share="true"
+        data-width="450"
+        data-show-faces="true">
+      </div>
+      <br>
+      <div class="fb-comments" data-href="http://skrzypczyk.fr" data-numposts="5" data-colorscheme="light"></div>
+      <br>
+
+      <?php
 
 
+        if($session)
+        {
+          
+          $token = (string) $session->getAccessToken();
+          $_SESSION['fb_token'] = $token;
 
-?>
+          //Prepare
+          $request = new FacebookRequest($session, 'GET', '/me');
+          //execute
+          $response = $request->execute();
+          //transform la data graphObject
+          $user = $response->getGraphObject("Facebook\GraphUser");
+          echo "<pre>";
+          print_r($user);
+          echo "</pre>";
+      
 
+        }else{
+          $loginUrl = $helper->getLoginUrl();
+          echo "<a href='".$loginUrl."'>Se connecter</a>";
+        }
 
-</body>
+          
+
+      ?>
+  </body>
 </html>
