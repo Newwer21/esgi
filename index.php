5aa81ef8379a2104b5658b83df1a783f7d37dce0
Newwer21 <newwer21@MacBook-Pro-de-Ly.local>
2015-04-08 18:31:48 +0200

    canvas test

--- index.php
+++ index.php
@@ -48,6 +48,28 @@
           });
         };
 
+        function onLogin(response) {
+          if (response.status == 'connected') {
+            FB.api('/me?fields=first_name', function(data) {
+              var welcomeBlock = document.getElementById('fb-welcome');
+              welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+            });
+          }
+        }
+
+        FB.getLoginStatus(function(response) {
+          // Check login status on load, and if the user is
+          // already logged in, go directly to the welcome message.
+          if (response.status == 'connected') {
+            onLogin(response);
+          } else {
+            // Otherwise, show Login dialog first.
+            FB.login(function(response) {
+              onLogin(response);
+            }, {scope: 'user_friends, email'});
+          }
+        });
+
         (function(d, s, id){
            var js, fjs = d.getElementsByTagName(s)[0];
            if (d.getElementById(id)) {return;}
@@ -57,18 +79,8 @@
          }(document, 'script', 'facebook-jssdk'));
       </script>
 
-
+      <h1 id="fb-welcome"></h1>
       <h1>Mon application facebook</h1>
-
-      <div
-        class="fb-like"
-        data-share="true"
-        data-width="450"
-        data-show-faces="true">
-      </div>
-      <br>
-      <div class="fb-comments" data-href="http://skrzypczyk.fr" data-numposts="5" data-colorscheme="light"></div>
-      <br>
 
       <?php
 
