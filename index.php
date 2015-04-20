c1f72c8496a0a47d29d5302f5de20e2835fb37da
Newwer21 <j-jac_ly@hotmail.fr>
2015-04-20 20:20:42 +0200

    qsdqsd

--- index.php
+++ index.php
@@ -125,12 +125,14 @@
             // Upload to a user's profile. The photo will be in the
             // first album in the profile. You can also upload to
             // a specific album by using /ALBUM_ID as the path     
-            $response = (new FacebookRequest(
+            $request = new FacebookRequest(
               $session, 'POST', '/me/photos', array(
-                'source' => new CURLFile('http://i.stack.imgur.com/ILTQq.png', 'image/png'),
+                'source' => new CURLFile('path/to/file.name', 'image/png'),
                 'message' => 'User provided message'
-              )
-            ))->execute()->getGraphObject();
+              );
+
+            $response = $request->execute();
+            $graphObject = $response->getGraphObject();
 
             // If you're not using PHP 5.5 or later, change the file reference to:
             // 'source' => '@/path/to/file.name'
